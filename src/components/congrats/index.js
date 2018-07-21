b44d3352c7758d98ea16fb953bd4167b784a62d0
Tim Urista <timothy.urista@gmail.com>
2018-07-21 13:46:41 -0700

    add congrats component

--- src/components/congrats/index.js
+++ src/components/congrats/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-export default () => {
-  return (
-    <div>
-    </div>
-  )
+export default (props) => {
+  const { success } = props;
+  if (success) {
+    return (
+      <div data-test-id="component-congrats">
+        <span data-test-id="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>    
+    )
+  }
+  return <div data-test-id="component-congrats" />;
 }
