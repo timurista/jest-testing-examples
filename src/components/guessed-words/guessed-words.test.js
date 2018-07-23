be64b37d2013d1f1e24e9eb2b37b0d01f779ca7d
Tim Urista <timothy.urista@gmail.com>
2018-07-22 20:50:30 -0700

    add tests for guessed word component

--- src/components/guessed-words/guessed-words.test.js
+++ src/components/guessed-words/guessed-words.test.js
@@ -4,8 +4,19 @@ import { shallow } from 'enzyme'
 import { findByTestAttr } from '../../test/test-utils';
 import GuessedWords from './'
 
+const defaultProps = {
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
+}
+
+const setup = (props={}) => {
+  const setupProps = {...defaultProps, ...props};
+  console.log('setupProps')
+  return shallow(<GuessedWords {...setupProps}/>)
+}
+
 describe('GuessedWords', () => {
-  it('renders', () => {
-    
+  it('renders without warning', () => {
+    const wrapper = setup();
+    expect(wrapper).toBeDefined();
   })
 })
