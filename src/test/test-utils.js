7180bd31d3460263b31b3c408feace0061d76139
Tim Urista <timothy.urista@gmail.com>
2018-07-23 07:12:19 -0700

    add component level tests for guessed word

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -1,4 +1,6 @@
 // @flow
-export const findByTestAttr = (wrapper, val) => {
+import type {ShallowWrapper} from 'enzyme'
+
+export const findByTestAttr = (wrapper: ShallowWrapper, val: string): ShallowWrapper => {
   return wrapper.find(`[data-test-id="${val}"]`)
 }
