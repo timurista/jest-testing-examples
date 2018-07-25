565efcb273b83eaad1796b65ba25984b89ade38c
Tim Urista <timothy.urista@gmail.com>
2018-07-25 06:51:54 -0700

    add test for success reducer when correct guess type is passed

---
+++ src/reducers/success-reducer.test.js
@@ -0,0 +1,12 @@
+import { actionTypes } from '../actions'
+import success from './success-reducer'
+
+test('returns default false when no action is passed', () => {
+  const newState = success();
+  expect(newState).toBe(false)
+})
+
+test('returns true when type correct guess is passed', () => {
+  const newState = success(undefined, { type: actionTypes.CORRECT_GUESS });
+  expect(newState).toBe(true)
+})
