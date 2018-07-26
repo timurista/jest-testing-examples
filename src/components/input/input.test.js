cbf74550ebda1b92ab13e26880b4aebedaca95d7
Tim Urista <timothy.urista@gmail.com>
2018-07-26 06:26:01 -0700

    add input component with basic tests

---
+++ src/components/input/input.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTesId } from '../../test/test-utils'
+import Input from './'
+
+describe('Input', () => {
+  describe('render', () => {
+    describe('word has not been guesed', () => {
+      it('renders without error', () => {
+
+      })
+      it('renders input box', () => {
+        
+      })
+      it('renders submit button', () => {
+        
+      })
+    })
+    describe('word has been guesed', () => {
+      it('renders without error', () => {
+
+      })
+      it('does not render input box', () => {
+        
+      })
+      it('does not render submit button', () => {
+        
+      })      
+    })
+  })
+
+  describe('update state', () => {
+    
+  })
+})
