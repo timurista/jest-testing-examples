b7b2f0082c3d89e492e536c0a6370a26237447de
Tim Urista <timothy.urista@gmail.com>
2018-07-29 10:16:40 -0700

    add tests for guessed word action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD'
 }
 
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
-}
+export const guessWord = (guessedWord) => {
+  return function(dispatch, getState) {
+
+  };
+};
+
+// export function correctGuess() {
+//   return { type: actionTypes.CORRECT_GUESS };
+// }
