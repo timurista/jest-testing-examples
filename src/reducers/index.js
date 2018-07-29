34e16b71691f0518a45b04218531fb2888976ae3
Tim Urista <timothy.urista@gmail.com>
2018-07-29 15:28:30 -0700

    add basic secret word reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import success from './success-reducer';
 import guessedWords from './guessed-words-reducer';
+import secretWord from './secret-word-reducer';
 
 
 export default combineReducers({
   success,
-  guessedWords
+  guessedWords,
+  secretWord
 })
