398a08c27b0a9bf86754cd2798ee8a1c1ef16cb7
Tim Urista <timothy.urista@gmail.com>
2018-07-30 07:00:01 -0700

    add integrations tests for different states with guessed words

--- src/integration.test.js
+++ src/integration.test.js
@@ -23,17 +23,55 @@ describe('guessWord action dispatcher', () => {
     })
 
     it('updates state for successful guess', () => {
-      
+      store.dispatch(guessWord(secretWord))
+      const newState = store.getState()
+      const expectedState = {
+        ...initialState,
+        success: true,
+        guessedWords: [{ 
+          guessedWord: secretWord, 
+          letterMatchCount: secretWord.length
+        }]
+      }
+      expect(newState).toEqual(expectedState)
     })
   })
 
   describe('some guessed words here', () => {
+    const guessedWords = [{
+      guessedWord: 'agile',
+      letterMatchCount: 1,
+    }]
+    const initialState = { guessedWords, secretWord }
+    let store;
+    beforeEach(() => {
+      store = storeFactory(initialState)
+    })
     it('updates state for unsuccessful guess', () => {
-
+      store.dispatch(guessWord(unsuccessfulGuess))
+      const newState = store.getState()
+      const expectedState = {
+        secretWord,
+        success: false,
+        guessedWords: [...guessedWords, { 
+          guessWord: unsuccessfulGuess, 
+          letterMatchCount: 3
+        }]
+      }
+      expect(newState).toBe(expectedState);        
     })
 
     it('updates state for successful guess', () => {
-      
+      store.dispatch(guessWord(secretWord))
+      const newState = store.getState()
+      const expectedState = {
+        success: true,
+        guessedWords: [...guessedWords, {
+          guessedWord: secretWord,
+          letterMatchCount: secretWord.length
+        }]
+      }
+      expect(newState).toEqual(expectedState)
     })
   })
 })
