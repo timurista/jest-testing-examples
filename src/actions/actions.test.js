08e9c52df08cec30e1484fcb3125f513a01086e5
Tim Urista <timothy.urista@gmail.com>
2018-07-30 19:59:43 -0700

    add moxios and axios and default actions test

---
+++ src/actions/actions.test.js
@@ -0,0 +1,16 @@
+import moxios from 'moxios';
+describe('getSecretWord action creator', () => {
+  beforeEach(() => {
+    moxios.install();
+  })
+
+
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it('adds response word to state', () => {
+    
+  })
+})
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../helpers'
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
-  GUESS_WORD: 'GUESS_WORD'
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 }
 
 export const guessWord = (guessedWord) => {
@@ -21,6 +23,12 @@ export const guessWord = (guessedWord) => {
     }
   };
 };
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    axios.get('http://localhost:3030');
+  }
+}
 
 // export function correctGuess() {
 //   return { type: actionTypes.CORRECT_GUESS };
