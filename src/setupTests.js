5c16a472e341c27cca520b6c00c0e30d66f36dd4
Tim Urista <timothy.urista@gmail.com>
2018-08-01 06:20:39 -0700

    add explicit test for component did mount lifecycle enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,7 @@
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import Enzyme from 'enzyme'
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+Enzyme.configure({ 
+  adapter: new EnzymeAdapter(),
+  disableLifecycleMethods: true 
+});
